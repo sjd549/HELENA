91f7f8a33d18915ff4a903d2de493c5c87f092ac
Ava Dean <ava.dean@york.ac.uk>
2026-03-26 19:32:14 +0000

    Change test to a parameterised test in anticipation of adding more varieties of input files to check.

--- tests/test_input.py
+++ tests/test_input.py
@@ -1,10 +1,13 @@
+import pytest
+
 from helena import run
 
 
-def test_AR2plus(snaptolshot, input_directory, work_env_with_chdir):
-    input_file = input_directory / "input1.toml"
-
-    run(argv=[str(input_file)])
+@pytest.mark.parametrize(
+    "input_file", ["input_tecplot2D.toml"],
+)
+def test_AR2plus(input_file, input_directory, snaptolshot, work_env_with_chdir):
+    run(argv=[str(input_directory / input_file)])
 
     output_file = work_env_with_chdir / "data/TECPlot2D/2DPlots_Data/AR2+.csv"
     assert output_file.exists()
